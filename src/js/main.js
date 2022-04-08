468718a81b1b71caa9f362c748c062f1f7301d58
Hùng Hoàng <hungcuong612001@gmail.com>
2022-04-08 09:45:10 +0700

    upload

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,5 @@
-var mySwiper = new Swiper ('.swiper-container', {
-    // Các Parameters
-    direction: 'vertical',
-    loop: true,
-
-    // Nếu cần pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-
-    // Nếu cần navigation
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-
-    // Nếu cần scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  })
+var buttonlist = document.querySelector('.header-icon i');
+var headerlist = document.querySelector('.header-list');
+buttonlist.addEventListener('click', () =>{
+  headerlist.classList.toggle('active');
+})
